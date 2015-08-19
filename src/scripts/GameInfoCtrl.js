4e14b1614be71f8e0e5997009161eee4ea16b192
Sanich <sancho@cdrpro.ru>
2015-08-19 16:16:25 +0300

    Added a timer to release game date

--- src/scripts/GameInfoCtrl.js
+++ src/scripts/GameInfoCtrl.js
@@ -3,6 +3,7 @@
 export default class GameInfoCtrl
 {
     ELEMENT_INFO_ID = '#game-info';
+    TIMER_CLASS = '.js-game-timer';
 
     game;
     el;
@@ -30,6 +31,7 @@ export default class GameInfoCtrl
             this.el.appendChild(this.game.renderInfo());
 
             this.renderChart();
+            this.renderTimer();
         });
     }
 
@@ -76,6 +78,48 @@ export default class GameInfoCtrl
             responsive: true,
             bezierCurve: false
         });
+    }
+
+    renderTimer()
+    {
+        if (!this.game.json.release_date) return;
+
+        let release_date = new Date(this.game.json.release_date);
+        if (release_date <= new Date()) return;
+
+        let second = 1000;
+        let minute = second * 60;
+        let hour = minute * 60;
+        let day = hour * 24;
+
+        let timer_el = this.el.querySelector(this.TIMER_CLASS);
+
+        let timer = window.setInterval(() => {
+
+            let now = new Date();
+            let distance = release_date - now;
+
+            if (distance < 0)
+            {
+                window.clearInterval(timer);
+                timer_el.textContent = 'Released!';
+                return;
+            }
+
+            let days = Math.floor(distance / day);
+
+            let hours = Math.floor((distance % day) / hour);
+            if (hours < 10) hours = `0${hours}`;
+
+            let minutes = Math.floor((distance % hour) / minute);
+            if (minutes < 10) minutes = `0${minutes}`;
+
+            let seconds = Math.floor((distance % minute) / second);
+            if (seconds < 10) seconds = `0${seconds}`;
+
+            timer_el.textContent = `${days} ${hours}:${minutes}:${seconds}`;
+
+        }, 1000);
     }
 
     renderLoader()
